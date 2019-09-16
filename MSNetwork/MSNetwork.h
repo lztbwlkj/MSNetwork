38a0c87f5f790aa97c59bdc81aa207875f6176a4
米山 <lztbwlkj@gmail.com>
2019-09-16 14:00:49 +0800

    将YYCache修改基于YYKit的版本库

--- MSNetwork/MSNetwork.h
+++ MSNetwork/MSNetwork.h
@@ -8,7 +8,6 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <YYKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -24,7 +23,7 @@ NS_ASSUME_NONNULL_BEGIN
 #define kIsWiFiNetwork [MSNetwork isWiFiNetwork]  // 一次性判断是否为WiFi网络的宏
 #endif
 
-@class YYCache, AFHTTPSessionManager;
+@class YYMemoryCache,YYDiskCache, AFHTTPSessionManager;
 /**
  这是我目前能想到的几个缓存场景
  * 只从网络获取数据，且数据不会缓存在本地（一般情况）
